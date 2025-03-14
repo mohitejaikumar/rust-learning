c42df204af3a287b78bec2d9a0bbb2d61ec6c0b4
mohitejaikumar <mohitejaikumar@gmail.com>
2025-03-14 19:10:41 +0530

    Learn Declarative and Procedural macro

--- assignment/src/main.rs
+++ assignment/src/main.rs
@@ -46,32 +46,64 @@
 //     sign_in_count: u64,
 // }
 
-use chrono::{Local, Utc};
+// use chrono::{Local, Utc};
 
-macro_rules! say_hello {
-    ($arg:expr) => {
-        println!("Hello, world {}", $arg);
-    };
-}
+// macro_rules! say_hello {
+//     ($arg:expr) => {
+//         println!("Hello, world {}", $arg);
+//     };
+// }
 
-fn main() {
-    // let mut user1 = User {
-    //     active: true,
-    //     sign_in_count: 1,
-    // };
+// macro_rules! create_function {
+//     ($func_name:ident) => {
+//         fn $func_name() {
+//             println!("Hello, world {}", stringify!($func_name));
+//         }
+//     };
+// }
 
-    // print_name(user1);
-    // print!("User 1 username: {}", user1.active); // Error - can not use borrowed value
+// create_function!(hello);
 
-    // let current_local_time = Local::now();
-    // println!("Current time: {}", current_local_time);
+// fn main() {
+//     // let mut user1 = User {
+//     //     active: true,
+//     //     sign_in_count: 1,
+//     // };
 
-    // let current_time = Utc::now();
-    // println!("Current time: {}", current_time);
+//     // print_name(user1);
+//     // print!("User 1 username: {}", user1.active); // Error - can not use borrowed value
 
-    say_hello!("jaikumar");
-}
+//     // let current_local_time = Local::now();
+//     // println!("Current time: {}", current_local_time);
+
+//     // let current_time = Utc::now();
+//     // println!("Current time: {}", current_time);
+
+//     say_hello!("jaikumar");
+//     hello();
+// }
 
 // fn print_name(user1: User) {
 //     print!("User 1 username: {}", user1.active);
 // }
+use std::fmt;
+#[derive(Debug)]
+struct User {
+    name: String,
+    age: u32,
+}
+
+// impl fmt::Display for User {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{} {}", self.name, self.age)
+//     }
+// }
+
+fn main() {
+    let user = User {
+        name: String::from("Jaikumar"),
+        age: 20,
+    };
+
+    println!("{:?}", user);
+}
